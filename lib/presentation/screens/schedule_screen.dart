21c158c3d2fdd33daaf9b97df18991ce87a4d640
Franks Gonzalez <gonzalezfranks@hotmail.com>
2025-03-12 13:05:36 +0100

    add: Eventos en el calendario (II)

--- lib/presentation/screens/schedule_screen.dart
+++ lib/presentation/screens/schedule_screen.dart
@@ -19,8 +19,8 @@ class ScheduleScreenState extends ConsumerState<ScheduleScreen> {
 
   //AHORA TOCA MONTAR ESTA PANTALLA!!! QUE ES DE LAS MÁS COMPLICADAS
 
-  DateTime focusedDay = DateTime.now();
-
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
 
 
   @override
@@ -32,6 +32,8 @@ class ScheduleScreenState extends ConsumerState<ScheduleScreen> {
     List<ClassModel> _getEventsForDay(DateTime day) {
       return classMap[DateTime(day.year, day.month, day.day)] ?? [];
     }
+
+
 
     for(var classModel in listaClases){
       //Convertimos el string date a DateTime (formato dd/mm/aaaa)
@@ -59,18 +61,105 @@ class ScheduleScreenState extends ConsumerState<ScheduleScreen> {
       body: Column(
         children: [
           TableCalendar(
-              focusedDay: focusedDay,
+              focusedDay: _focusedDay,
             firstDay: DateTime.utc(2025, 1, 1),
             lastDay: DateTime.utc(2035, 12, 31),
             //locale: "es_ES",
             calendarFormat: CalendarFormat.month,
             startingDayOfWeek: StartingDayOfWeek.monday,
-            /*
-              selectedDayPredicate: (day){
-                return isSameDay(a, b)
+
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+
+            onDaySelected: (selectedDay, focusedDay){
+              if(!isSameDay(_selectedDay, selectedDay)){
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+
+                  /*
+                void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+                  if (!isSameDay(_selectedDay, selectedDay)) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                      _selectedEvents.value = _getEventsForDay(selectedDay);
+                    });
+                  }
+                }
+                 */
+
+                });
+              }
             },
-            */
+
             eventLoader: _getEventsForDay,
+            calendarStyle: const CalendarStyle(
+              outsideDaysVisible: false,
+              weekendTextStyle: TextStyle(color: Colors.pink),
+              holidayTextStyle: TextStyle(color: Colors.pink),
+            ),
+            headerStyle: const HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false
+            ),
+            calendarBuilders: CalendarBuilders(
+              selectedBuilder: (context, date, _){
+                return Container(
+                  margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.only(top: 5.0, left: 6.0),
+                  color: Colors.yellow,
+                  width: 100,
+                  height: 100,
+                  child: Text(
+                    "${date.day}",
+                    style: const TextStyle().copyWith(fontSize: 16.0),
+                  ),
+                );
+              },
+
+              todayBuilder: (context, date, _) {
+                return Container(
+                  margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.only(top: 5.0, left: 6.0),
+                  color: Colors.teal,
+                  width: 100,
+                  height: 100,
+                  child: Text(
+                    "${date.day}",
+                    style: const TextStyle()
+                        .copyWith(fontSize: 16.0, color: Colors.white),
+                  ),
+                );
+              },
+                /*
+              prioritizedBuilder: (context, date, _){
+                return Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.zero,
+                      color: Colors.red,
+                      border: Border.all(
+                          color: Colors.black,
+                          width: 2.0
+                      )
+                  ),
+                  child: Center(
+                    child: Text(
+                      "${date.day}",
+                      style: TextStyle().copyWith(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white
+                      ),
+                    ),
+                  ),
+                );
+              }
+              */
+            ),
 
           ),
 
@@ -80,27 +169,63 @@ class ScheduleScreenState extends ConsumerState<ScheduleScreen> {
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  //Contenido
-                  const Row(
+
+                  Row(
                     children: [
-                      Spacer(),
-                      Text("2 Clases")
+                      const Spacer(),
+                      Text("${_getEventsForDay(_selectedDay).length} Clases")
                     ],
                   ),
+
+
+                  ListView.builder(
+                    shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _getEventsForDay(_selectedDay).length,
+                      itemBuilder: (context, index){
+
+                      List<ClassModel> clasesDelDia = _getEventsForDay(_selectedDay);
+
+                      return Column(
+                        children: [
+                          //Contenido
+
+                          Row(
+                            children: [
+                              Text("${index + 1}. Clase"),
+                              const Spacer(),
+                              Text("${clasesDelDia[index].hour}")
+                            ],
+                          ),
+
+                          const Row(
+                            children: [
+                              Text("Asistentes:")
+                            ],
+                          ),
+
+                          ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: clasesDelDia[index].amountStudents,
+                              itemBuilder: (context, index){
+
+                                //Aqui tengo que rellenar con la informacion de los estudiantes.
+
+                                return EmptyStudentWidget();
+                              }
+                          ),
+
+                        ],
+                      );
+                      }
+                  ),
+
+
             
-                  const Row(
-                    children: [
-                      Text("1. Clase"),
-                      Spacer(),
-                      Text("17:00 - 18:45")
-                    ],
-                  ),
+
             
-                  const Row(
-                    children: [
-                      Text("Asistentes:")
-                    ],
-                  ),
+
 
                   ListView.builder(
                     shrinkWrap: true,
